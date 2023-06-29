7865a51ed4dab5061c59e3922f9a0e5aea5598b0
Aznaggar <bobby.bounty@protonmail.com>
2023-06-29 19:30:27 +0200

    [Poly] return obj copy instead of obj& > op(+, -)
    Added pre-defined polynomials
    Added description for basic poly addition and substraction test cases
    Polynomial.simplify() now removes 0*x^n polynomials

--- main.cpp
+++ main.cpp
@@ -5,47 +5,64 @@
 
 void testing()
 {
-    obj::polynomial::Polynomial<> poly({
-        {7u, 1},
-        {4u, -2},
-        {9u, 3},
-        {9u, 1},
-    });
-    obj::polynomial::Polynomial<> poly2({
-        {4u, 1},
-        {3u, -2},
-        {2u, 3},
-        {1u, 1},
-    });
-    // poly.debug_printArgs();
-    // poly.simplify();
-    // -poly;
-    // poly.debug_printArgs();
-    // poly2.debug_printArgs();
-    // auto poly3 = (poly + poly2) - poly2;
-    // poly3.debug_printArgs();
-    // poly3.simplify();
-    // poly3.debug_printArgs();
-    // auto poly4 = poly - poly2;
-    // poly4.debug_printArgs();
-    // poly4.simplify();
-    // poly4.debug_printArgs();
-
-    obj::polynomial::Polynomial<> polyForMul({
-        {2u, 1},
-        {3u, -2},
-    });
-    obj::polynomial::Polynomial<> polyForMul2({
-        {2u, 3},
-        {1u, 1},
-    });
-    polyForMul.debug_printArgs();
-    polyForMul2.debug_printArgs();
-    auto polyMulRes = polyForMul * polyForMul2;
-    polyMulRes.debug_printArgs();
-    polyMulRes.simplify();
-    polyMulRes.debug_printArgs();
-
+    auto polyFirst = []()
+    {
+        obj::polynomial::Polynomial<> poly({
+            {7u, 1},
+            {4u, -2},
+            {9u, 3},
+            {9u, 1},
+        });
+        obj::polynomial::Polynomial<> poly2({
+            {4u, 1},
+            {3u, -2},
+            {2u, 3},
+            {1u, 1},
+        });
+        std::cout << "Poly #1" << std::endl;
+        poly.debug_printArgs();
+        std::cout << "Poly #1.simplify()" << std::endl;
+        poly.simplify();
+        poly.debug_printArgs();
+        std::cout << "pPoly = +(Poly #1); Usage of: unary(+)" << std::endl;
+        auto pPoly = +poly;
+        pPoly.debug_printArgs();
+        std::cout << "nPoly = -(Poly #1); Usage of: unary(-)" << std::endl;
+        auto nPoly = -poly;
+        nPoly.debug_printArgs();
+        std::cout << "Poly #2" << std::endl;
+        poly2.debug_printArgs();
+        std::cout << "Poly #3 = (Poly #1 + Poly #2) + Poly #2 - Poly #1; Usage of: binary(+), binary(-)" << std::endl;
+        auto poly3 = (poly + poly2) - poly2 - poly;
+        poly3.debug_printArgs();
+        std::cout << "Poly #3.simplify()" << std::endl;
+        poly3.simplify();
+        poly3.debug_printArgs();
+        std::cout << "Poly #3 = Poly #1 - Poly #2; Usage of: binary(-)" << std::endl;
+        auto poly4 = poly - poly2;
+        poly4.debug_printArgs();
+        std::cout << "Poly #3.simplify()" << std::endl;
+        poly4.simplify();
+        poly4.debug_printArgs();
+    };
+    auto polyMul = []()
+    {
+        obj::polynomial::Polynomial<> polyForMul({
+            {2u, 1},
+            {3u, -2},
+        });
+        obj::polynomial::Polynomial<> polyForMul2({
+            {2u, 3},
+            {1u, 1},
+        });
+        polyForMul.debug_printArgs();
+        polyForMul2.debug_printArgs();
+        auto polyMulRes = polyForMul * polyForMul2;
+        polyMulRes.debug_printArgs();
+        polyMulRes.simplify();
+        polyMulRes.debug_printArgs();
+    };
+    polyFirst();
     // std::cout << poly.call(2.02f) << std::endl;
     // std::cout << poly.strigify() << std::endl;
 }
--- pre.d/polynomials.h
+++ pre.d/polynomials.h
@@ -6,26 +6,12 @@
 #include <array>
 
 using obj::polynomial::Polynomial;
-using obj::polynomial::coeffDefType;
-using obj::polynomial::powerDefType;
 
 namespace pre::polynomials
 {
-    // using argType  = std::pair<coeffDefType, powerDefType>;
-    // using list = std::list<std::pair<coeffDefType, powerDefType>>;
-    // // using array = std::array<std::pair<coeffDefType, powerDefType>>;
-
-    // // static constexpr inline const argType x_arg ;
-    // // static constexpr inline const argType x2_arg{std::make_pair(1, 1u)};
-    // // static constexpr inline const argType x3_arg{std::make_pair(1, 1u)};
-
-    // constexpr std::array<std::pair<coeffDefType, powerDefType>, 1> args = {{1, 1u}};
-    // static constexpr inline const auto x2_arg_list {std::make_pair(1, 2u)};
-    // static constexpr inline const auto x3_arg_list {std::make_pair(1, 3u)};
-
-    // static constexpr inline const Polynomial<> x  (x_arg_list);
-    // static constexpr inline const Polynomial<> x2 ({std::make_pair(1, 2u)});
-    // static constexpr inline const Polynomial<> x3 ({std::make_pair(1, 3u)});
+    static /* constexpr */ inline const Polynomial<> x  ({{1u, 1}});
+    static /* constexpr */ inline const Polynomial<> x2 ({{1u, 2}});
+    static /* constexpr */ inline const Polynomial<> x3 ({{1u, 3}});
 }
 
 #endif
